7424079c4fcae6313848ff4d732fd75ea478fa48
PouPeuu <qwertydude277@gmail.com>
2024-07-16 15:07:46 +0300

    Add modlist

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -18,8 +18,54 @@ string read_file(string path) {
     return text;
 } */
 
-MainWindow::MainWindow() {
+MainWindow::MainWindow()
+:   mainbox(Gtk::Orientation::HORIZONTAL),
+    modlist_box(Gtk::Orientation::VERTICAL),
+    modlist_heading("<b>Mods:</b>", Gtk::Align::START) {
+    
     set_title("Primitive Wings Mod Loader");
     set_default_size(600, 400);
     
+    set_child(mainbox);
+
+    mainbox.append(modlist_box);
+
+    modlist_heading.set_use_markup();
+    modlist_box.set_margin(10);
+    modlist_box.append(modlist_heading);
+    modlist_scrollwindow.set_child(modlist_view);
+
+    modlist_scrollwindow.set_policy(Gtk::PolicyType::AUTOMATIC, Gtk::PolicyType::AUTOMATIC);
+    modlist_scrollwindow.set_expand();
+
+    modlist_box.append(modlist_scrollwindow);
+    
+    modlist_names = Gtk::StringList::create({"bestmod", "Jurt's epic mod", "Heketiitus' vomit inducingly bad mod", "Industrialcrap"});
+
+    auto selection_model = Gtk::SingleSelection::create(modlist_names);
+    selection_model->set_autoselect(false);
+    selection_model->set_can_unselect(true);
+    modlist_view.set_model(selection_model);
+    modlist_view.add_css_class("data-table");
+
+    auto factory = Gtk::SignalListItemFactory::create();
+    factory->signal_setup().connect(
+        sigc::mem_fun(*this, &MainWindow::on_setup_label));
+    factory->signal_bind().connect(
+        sigc::mem_fun(*this, &MainWindow::on_bind_name));
+    modlist_view.set_factory(factory);
+}
+
+void MainWindow::on_setup_label(const Glib::RefPtr<Gtk::ListItem>& list_item) {
+    list_item->set_child(*Gtk::make_managed<Gtk::Label>("", Gtk::Align::START));
+}
+
+void MainWindow::on_bind_name(const Glib::RefPtr<Gtk::ListItem>& list_item) {
+    auto pos = list_item->get_position();
+    if (pos == GTK_INVALID_LIST_POSITION)
+        return;
+    auto label = dynamic_cast<Gtk::Label*>(list_item->get_child());
+    if (!label)
+        return;
+    label->set_text(modlist_names->get_string(pos));
 }
--- MainWindow.h
+++ MainWindow.h
@@ -6,6 +6,16 @@
 
 class MainWindow : public Gtk::Window {
 	protected:
+		void on_setup_label(const Glib::RefPtr<Gtk::ListItem>& list_item);
+		void on_bind_name(const Glib::RefPtr<Gtk::ListItem>& list_item);
+
+		Gtk::Box mainbox;
+		Gtk::Box modlist_box;
+		Gtk::Label modlist_heading;
+		Gtk::ScrolledWindow modlist_scrollwindow;
+		Gtk::ListView modlist_view;
+
+		Glib::RefPtr<Gtk::StringList> modlist_names;
 	public:
 		MainWindow();
 };

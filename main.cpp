7cd819a917e6a6c8ca58dac044223def7927876c
PouPeuu <qwertydude277@gmail.com>
2024-07-18 16:12:49 +0300

    Load mods on startup

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 
 int main(int argc, char *argv[]) {
 	std::filesystem::current_path(Utils::get_executable_dir());
-	
+	Mods::load_from_folder("mods");
+		
 	auto app = Gtk::Application::create("com.poupeuu.pwml");
 	return app->make_window_and_run<MainWindow>(argc, argv);
 }

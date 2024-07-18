59a8c381647a46dbfc1ce82cd752d9f0e24c22ca
PouPeuu <qwertydude277@gmail.com>
2024-07-18 15:12:03 +0300

    Create utils for file management

---
+++ Utils.cpp
@@ -0,0 +1,79 @@
+#include "Utils.h"
+
+std::string read_file(std::string filepath) {
+    std::ifstream file(filepath);
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening file " << filepath << std::endl;
+        return NULL;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+
+    std::string contents = buffer.str();
+    file.close();
+    return contents;
+}
+
+std::vector<std::string> list_files(std::string path) {
+    std::vector<std::string> paths;
+    try {
+        for (const auto& entry : std::filesystem::directory_iterator(path)) {
+            paths.push_back(entry.path());
+        }
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    return paths;
+}
+
+
+
+#if defined(_WIN32)
+#include <windows.h>
+#include <algorithm>
+
+std::string get_executable_dir() {
+    char buffer[MAX_PATH];
+    GetModuleFileName(NULL, buffer, MAX_PATH);
+    std::string path(buffer);
+    size_t pos = path.find_last_of("\\/");
+    return path.substr(0, pos);
+}
+
+#elif defined(__linux__)
+#include <unistd.h>
+#include <limits.h>
+
+std::string get_executable_dir() {
+    char buffer[PATH_MAX];
+    ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer) - 1);
+    if (len != -1) {
+        buffer[len] = '\0';
+        std::string path(buffer);
+        size_t pos = path.find_last_of("/");
+        return path.substr(0, pos);
+    }
+    return std::string();
+}
+
+#elif defined(__APPLE__)
+#include <mach-o/dyld.h>
+#include <vector>
+
+std::string get_executable_dir() {
+    uint32_t size = 0;
+    _NSGetExecutablePath(nullptr, &size);  // Get the size needed
+    std::vector<char> buffer(size);
+    if (_NSGetExecutablePath(buffer.data(), &size) == 0) {
+        std::string path(buffer.begin(), buffer.end());
+        size_t pos = path.find_last_of("/");
+        return path.substr(0, pos);
+    }
+    return std::string();
+}
+
+#else
+#error "Unsupported platform"
+#endif
---
+++ Utils.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+#include <vector>
+
+std::string read_file(std::string filepath);
+std::vector<std::string> list_files(std::string filepath);
+std::string get_executable_dir();
+
+#endif

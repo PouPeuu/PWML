aef959629b0961894319bc8a504e6fc1abc006d1
PouPeuu <qwertydude277@gmail.com>
2024-07-18 15:12:46 +0300

    Set working directory to executable directory to avoid bullshit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "MainWindow.h"
+#include "Mods.h"
 
 int main(int argc, char *argv[]) {
+	std::filesystem::current_path(get_executable_dir());
+	
 	auto app = Gtk::Application::create("com.poupeuu.pwml");
 	return app->make_window_and_run<MainWindow>(argc, argv);
 }

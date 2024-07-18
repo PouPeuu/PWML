81394968b8e731b37b503cf764538a5b315bfc51
PouPeuu <qwertydude277@gmail.com>
2024-07-18 18:37:54 +0300

    Add checks and shit to the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+if [ ! -d build/ ]; then
+    mkdir build
+fi
 if [ "$1" == "linux" ]; then
     g++ -o build/PWML *.cpp $(pkg-config gtkmm-4.0 --cflags --libs)
 fi
 if [ "$1" == "windows" ]; then
     x86_64-w64-mingw32-g++ -o build/PWML.exe *.cpp $(pkg-config gtkmm-4.0 --cflags --libs)
 fi
+cp style.css build/style.css

9c395df8d1d74ab9d13413017c187d651b94cf11
PouPeuu <qwertydude277@gmail.com>
2024-07-20 23:43:40 +0300

    no more iwnodwsefwgzsrfhdtgjykuji AAAAAAAAAAa

--- build.sh
+++ build.sh
@@ -2,10 +2,5 @@
 if [ ! -d build/ ]; then
     mkdir build
 fi
-if [ "$1" == "linux" ]; then
-    g++ -o build/PWML *.cpp $(pkg-config gtkmm-4.0 tinyxml2 --cflags --libs)
-fi
-if [ "$1" == "windows" ]; then
-    x86_64-w64-mingw32-g++ -o build/PWML.exe *.cpp $(pkg-config gtkmm-4.0 tinyxml2 --cflags --libs)
-fi
+g++ -o build/PWML *.cpp $(pkg-config gtkmm-4.0 tinyxml2 --cflags --libs) $2 -std=c++20
 cp style.css build/style.css

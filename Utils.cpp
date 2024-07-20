34803bcaeb3a1714f262f930c3d04d659970a0a4
PouPeuu <qwertydude277@gmail.com>
2024-07-20 17:01:02 +0300

    Create Utils::clome_xml and shorten std::filesystem to fs

--- Utils.cpp
+++ Utils.cpp
@@ -1,6 +1,6 @@
 #include "Utils.h"
 
-std::string Utils::read_file(std::filesystem::path filepath) {
+std::string Utils::read_file(fs::path filepath) {
     std::ifstream file(filepath);
 
     if (!file.is_open()) {
@@ -16,16 +16,38 @@ std::string Utils::read_file(std::filesystem::path filepath) {
     return contents;
 }
 
-std::vector<std::filesystem::path> Utils::list_files(std::filesystem::path path) {
-    std::vector<std::filesystem::path> paths;
+std::vector<fs::path> Utils::list_files(fs::path path) {
+    std::vector<fs::path> paths;
     try {
-        for (const auto& entry : std::filesystem::directory_iterator(path)) {
+        for (const auto& entry : fs::directory_iterator(path)) {
             paths.push_back(entry.path());
         }
-    } catch (const std::filesystem::filesystem_error& e) {
+    } catch (const fs::filesystem_error& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
     return paths;
+}
+
+void Utils::clone_xml(fs::path from, fs::path to, std::string root_name) {
+    if (!fs::exists(to)) {
+        fs::copy(from, to);
+        return;
+    }
+    xml::XMLDocument fromdoc;
+    xml::XMLError fromresult = fromdoc.LoadFile(from.string().c_str());
+    xml::XMLDocument todoc;
+    xml::XMLError toresult = todoc.LoadFile(to.string().c_str());
+
+    xml::XMLNode* fromroot = fromdoc.FirstChildElement(root_name.c_str());
+    xml::XMLNode* toroot = todoc.FirstChildElement(root_name.c_str());
+
+    xml::XMLElement* element = fromroot->FirstChildElement();
+    while (element != nullptr) {
+        toroot->InsertEndChild(element->DeepClone(&todoc));
+        element = element->NextSiblingElement();
+    }
+
+    xml::XMLError result = todoc.SaveFile(to.string().c_str());
 }
 
 
@@ -34,7 +56,7 @@ std::vector<std::filesystem::path> Utils::list_files(std::filesystem::path path)
 #include <windows.h>
 #include <algorithm>
 
-std::filesystem::path Utils::get_executable_dir() {
+fs::path Utils::get_executable_dir() {
     char buffer[MAX_PATH];
     GetModuleFileName(NULL, buffer, MAX_PATH);
     std::string path(buffer);
@@ -46,7 +68,7 @@ std::filesystem::path Utils::get_executable_dir() {
 #include <unistd.h>
 #include <limits.h>
 
-std::filesystem::path Utils::get_executable_dir() {
+fs::path Utils::get_executable_dir() {
     char buffer[PATH_MAX];
     ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer) - 1);
     if (len != -1) {
@@ -62,7 +84,7 @@ std::filesystem::path Utils::get_executable_dir() {
 #include <mach-o/dyld.h>
 #include <vector>
 
-std::filesystem::path Utils::get_executable_dir() {
+fs::path Utils::get_executable_dir() {
     uint32_t size = 0;
     _NSGetExecutablePath(nullptr, &size);  // Get the size needed
     std::vector<char> buffer(size);
--- Utils.h
+++ Utils.h
@@ -7,10 +7,16 @@
 #include <sstream>
 #include <filesystem>
 #include <vector>
+#include <tinyxml2.h>
+
+namespace xml = tinyxml2;
+
+namespace fs = std::filesystem;
 
 namespace Utils {
-    std::string read_file(std::filesystem::path filepath);
-    std::vector<std::filesystem::path> list_files(std::filesystem::path filepath);
-    std::filesystem::path get_executable_dir();
+    std::string read_file(fs::path filepath);
+    std::vector<fs::path> list_files(fs::path filepath);
+    fs::path get_executable_dir();
+    void clone_xml(fs::path from, fs::path to, std::string root_name);
 }
 #endif
